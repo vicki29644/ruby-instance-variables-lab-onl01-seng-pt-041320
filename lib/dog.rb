75683247dab8ef36d7e1565b00277bbcb440737d
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-30 23:47:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,15 @@
-class_Dog
-def name=(dogs_name)
-  @this_Dogs_name=
-  dogs_name
- end
- 
- Def name
-  @this_Dogs_name
-   end
-end
-lassie = Dog.new
-lassie.name ="Lassie"
+class Dog
 
- puts lassie.name
- 
- 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+
+  def name
+    @this_dogs_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
