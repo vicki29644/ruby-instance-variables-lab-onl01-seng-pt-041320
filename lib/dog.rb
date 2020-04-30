6b5cabc1e6a13bf3247d7d0f087d7c185fa1281e
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-30 18:58:03 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class_Dog
+def name=(Lassie_name)
+  this_Dogs_name=Lassie
+  Dog_Lassie
+ end
+ 
+ Def Lassie
+  this_Dogs_name
+end
+end
+Lassie = Dog.new
+lassie.name=("Lassie")
+
+ puts lassie.name
